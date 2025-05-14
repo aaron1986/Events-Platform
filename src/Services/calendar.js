090dc3be1f42a848e5d919a13ef3fd6791742d28
aaron1986 <aaronbrucesmith86@gmail.com>
2025-05-14 14:29:53 +0100

    corrrected the .env file

--- src/Services/calendar.js
+++ src/Services/calendar.js
@@ -1,7 +1,8 @@
 export function addToCalendar(event) {
   window.gapi.load('client:auth2', () => {
+    const apiKey = import.meta.env.VITE_CALENDAR_API_KEY;
     window.gapi.client.init({
-      apiKey: "AIzaSyDyoAQkGMmRQD2-PYagr3Q4Wq9lEV7Wt-I",
+      apiKey: apiKey,
       clientId: "924240908915-b1tobmtfd7kbkc8fvmq3ubnj0jp1lk6c.apps.googleusercontent.com",
       discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
       scope: "https://www.googleapis.com/auth/calendar.events",
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,2 +1,12 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+test.describe("Content Page", () => {
+  test.beforeEach(async ({page}) =>{
+    await page.goto('https://playful-lebkuchen-6de85c.netlify.app/')
+  })
+
+test.skip("TITLE PAGE", async({page, request}) => {
+      await expect(page).toHaveTitle("Northcoders Event Platform");
+})
+
+})
